229368fccf40277ef57c9719b86b401da22d4f98
nachoperna <ignacioperna@gmail.com>
2025-10-12 20:24:55 -0300

    manejo de todas las rutas y redirecciones necesarias para el correcto funcionamiento de la app

---
+++ TP_Especial/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	sqlc "TP_Especial/db/sqlc"
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var queries *sqlc.Queries
+var ctx context.Context
+
+func main() {
+	// Servidor de archivos
+	fs := http.FileServer(http.Dir("static/"))
+
+	// Conexion a la Base de Datos PostgreSQL
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=nachoperna password=nachobdtpe dbname=BD_TPEspecial sslmode=disable")
+	if err != nil {
+		log.Fatalf("Error al conectar con la Base de Datos: %v", err)
+	}
+	defer db.Close()
+	fmt.Println("✅ Conectado a PostgreSQL correctmente")
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Verificar si el archivo existe físicamente
+		requestedPath := filepath.Join("static", r.URL.Path)
+		_, err := os.Stat(requestedPath)
+		if os.IsNotExist(err) {
+			// Archivo no existe, servir página personalizada
+			http.ServeFile(w, r, "static/ruta_invalida.html")
+			return
+		}
+		// Si existe, usar FileServer normalmente
+		fs.ServeHTTP(w, r)
+	}))
+
+	queries = sqlc.New(db)
+	ctx = context.Background()
+
+	// Procesamiento del formulario de LOGIN
+	http.HandleFunc("/login-confirmation", confirmLogin)
+
+	// Procesamiento del formulario de REGISTRO
+	http.HandleFunc("/register-confirmation", confirmRegiser)
+
+	// Servir Home del usuario
+	http.HandleFunc("/bienvenida", showHome)
+
+	// Procesamiento de DEPOSITO
+	http.HandleFunc("/deposit", deposit)
+
+	// Procesamiento de RETIRO
+	http.HandleFunc("/withdrawal", withdrawal)
+
+	// Procesamiento de TRANSFERENCIA
+	http.HandleFunc("/transfer", transfer)
+
+	fmt.Print("Servidor escuchando en puerto :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func showHome(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	balance, err := queries.GetBalance(ctx, values.Get("alias"))
+	if err != nil {
+		http.Error(w, "Error al obtener datos de cuenta", http.StatusInternalServerError)
+		return
+	}
+	datos := map[string]string{
+		"Alias":            values.Get("alias"),
+		"Balance":          balance.Balance,
+		"LastMovementType": balance.LastMovementType.String,
+	}
+
+	var user sqlc.User
+	if values.Get("name") == "" {
+		user, _ = queries.GetUser(ctx, values.Get("alias"))
+		datos["Name"] = user.Name
+		datos["Email"] = user.Email
+	} else {
+		datos["Name"] = values.Get("name")
+		datos["Email"] = values.Get("email")
+	}
+
+	// Servir el template con datos actualizados
+	tmp, err := template.ParseFiles("static/bienvenida.html")
+	if err != nil {
+		http.Error(w, "Error al cargar template", http.StatusInternalServerError)
+		return
+	}
+	tmp.Execute(w, datos)
+}
+
+func confirmLogin(w http.ResponseWriter, r *http.Request) {
+	datos := map[string]string{
+		"Alias":    r.FormValue("alias"),
+		"Password": r.FormValue("password"),
+	}
+
+	user, err := queries.GetUser(ctx, datos["Alias"])
+	if err == sql.ErrNoRows {
+		http.Redirect(w, r, "/?error=alias_not_found", http.StatusSeeOther)
+		return
+	}
+	if user.Password != datos["Password"] {
+		http.Redirect(w, r, "/?error=password_incorrect", http.StatusSeeOther)
+		return
+	}
+
+	redirectURL := fmt.Sprintf("/bienvenida?alias=%s&name=%s&email=%s",
+		user.Alias,
+		user.Name,
+		user.Email)
+
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func confirmRegiser(w http.ResponseWriter, r *http.Request) {
+	datos := map[string]string{
+		"Alias":    r.FormValue("alias"),
+		"Name":     r.FormValue("name"),
+		"Email":    r.FormValue("email"),
+		"Password": r.FormValue("password"),
+	}
+
+	_, err := queries.GetUser(ctx, datos["Alias"])
+	if err == sql.ErrNoRows {
+		err = queries.InsertUser(ctx, sqlc.InsertUserParams{
+			Alias:    datos["Alias"],
+			Name:     datos["Name"],
+			Email:    datos["Email"],
+			Password: datos["Password"]})
+		if err != nil {
+			http.Error(w, "Error al insertar usuario en la base", http.StatusInternalServerError)
+		}
+	} else {
+		http.Error(w, "El alias ya se encuentra registrado en la Base. Por favor elija otro", http.StatusInternalServerError)
+		return
+	}
+
+	redirectURL := fmt.Sprintf("/bienvenida?alias=%s&name=%s&email=%s",
+		datos["Alias"],
+		datos["Name"],
+		datos["Email"])
+
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func deposit(w http.ResponseWriter, r *http.Request) {
+	datos := map[string]string{
+		"Alias":  r.FormValue("alias"),
+		"Amount": r.FormValue("amount"),
+	}
+	amount, err := strconv.ParseFloat(datos["Amount"], 64)
+	if err != nil {
+		http.Error(w, "Monto inválido", http.StatusBadRequest)
+		return
+	}
+
+	_, err = queries.Deposit(ctx, sqlc.DepositParams{
+		Alias:             datos["Alias"],
+		LastDepositAmount: sql.NullString{String: fmt.Sprintf("%.2f", amount), Valid: true},
+	})
+
+	if err != nil {
+		http.Error(w, "Error al Depositar", http.StatusInternalServerError)
+		return
+	}
+
+	redirectURL := fmt.Sprintf("/bienvenida?alias=%s",
+		datos["Alias"])
+
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func transfer(w http.ResponseWriter, r *http.Request) {
+	// POR EL MOMMENTO EL USUARIO DEBE INGRESAR SU PROPIO ALIAS
+	datos := map[string]string{
+		"Alias_propio": r.FormValue("own_alias"),
+		"Alias_otro":   r.FormValue("other_alias"),
+		"Amount":       r.FormValue("amount"),
+	}
+	amount, err := strconv.ParseFloat(datos["Amount"], 64)
+	if err != nil {
+		http.Error(w, "Monto inválido", http.StatusBadRequest)
+		return
+	}
+
+	_, err = queries.GetUser(ctx, datos["Alias_otro"])
+	if err == sql.ErrNoRows {
+		http.Error(w, "La alias destino no esta registrado en el sistema", http.StatusInternalServerError)
+		return
+	}
+	err = queries.Transfer(ctx, sqlc.TransferParams{
+		Alias:               datos["Alias_propio"],
+		LastTransferAccount: sql.NullString{String: datos["Alias_otro"], Valid: true},
+		LastTransferAmount:  sql.NullString{String: fmt.Sprintf("%.2f", amount), Valid: true},
+	})
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		http.Error(w, "Error al quitar dinero de la cuenta de origen", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = queries.Deposit(ctx, sqlc.DepositParams{
+		Alias:             datos["Alias_otro"],
+		LastDepositAmount: sql.NullString{String: fmt.Sprintf("%.2f", amount), Valid: true},
+	})
+	if err != nil {
+		http.Error(w, "Error al depositar dinero en la cuenta destino", http.StatusInternalServerError)
+		return
+	}
+
+	redirectURL := fmt.Sprintf("/bienvenida?alias=%s",
+		datos["Alias_propio"])
+
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func withdrawal(w http.ResponseWriter, r *http.Request) {
+	// POR EL MOMMENTO EL USUARIO DEBE INGRESAR SU PROPIO ALIAS
+	datos := map[string]string{
+		"Alias":  r.FormValue("alias"),
+		"Amount": r.FormValue("amount"),
+	}
+	amount, err := strconv.ParseFloat(datos["Amount"], 64)
+	if err != nil {
+		http.Error(w, "Monto inválido", http.StatusBadRequest)
+		return
+	}
+
+	err = queries.Withdrawal(ctx, sqlc.WithdrawalParams{
+		Alias:                datos["Alias"],
+		LastWithdrawalAmount: sql.NullString{String: fmt.Sprintf("%.2f", amount), Valid: true},
+	})
+	if err != nil {
+		http.Error(w, "Error al quitar dinero de la cuenta origen", http.StatusInternalServerError)
+		return
+	}
+
+	redirectURL := fmt.Sprintf("/bienvenida?alias=%s&name=%s&email=%s",
+		datos["Alias"])
+
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+// func newPassword(w http.ResponseWriter, r *http.Request){
+//
+// }
