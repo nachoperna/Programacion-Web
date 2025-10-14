6f34553ca39eec25d99309b9502eb9b23dd8fef0
nachoperna <ignacioperna@gmail.com>
2025-10-14 18:27:19 -0300

    update, delete e insert retornan el usuario

--- TP_Especial/db/queries/users.sql
+++ TP_Especial/db/queries/users.sql
@@ -1,19 +1,22 @@
 -- name: GetUser :one
 select * from users where alias = $1;
 
--- name: DeleteUser :exec
-delete from users where alias = $1;
+-- name: DeleteUser :one
+delete from users where alias = $1
+returning *;
 
 -- name: ListUsers :many 
 select * from users;
 
--- name: InsertUser :exec
+-- name: InsertUser :one
 insert into users 
       (alias, name, email, password)
-      values ($1, $2, $3, $4);
+      values ($1, $2, $3, $4)
+returning *;
 
--- name: UpdateUser :exec
-update users set name = $2, email = $3, password = $4 where alias = $1;
+-- name: UpdateUser :one
+update users set name = $2, email = $3, password = $4 where alias = $1
+returning *;
 
 -- name: DeleteAllUsers :exec
 delete from users;
