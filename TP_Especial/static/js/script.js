17c46ae1db88604765fd3723cd29fcb1b783795e
nachoperna <ignacioperna@gmail.com>
2025-10-24 14:14:27 -0300

    uso de fetch para tabla dinamica de pedidos recibidos y realizados con eventos de click en botones

--- TP_Especial/static/js/script.js
+++ TP_Especial/static/js/script.js
@@ -1,39 +1,106 @@
-function displayLogin(){
+function displayLogin() {
       document.getElementById("register-form").style.display = "none";
       document.getElementById("login-form").style.display = "block";
 }
-function displayRegister(){
+function displayRegister() {
       document.getElementById("login-form").style.display = "none";
       document.getElementById("register-form").style.display = "block";
 }
 
-function displayNewPass(){
-      if (!document.getElementById("new-password").classList.contains('new-pass-unactive')){
+function displayNewPass() {
+      if (!document.getElementById("new-password").classList.contains('new-pass-unactive')) {
             document.getElementById("new-password").classList.add('new-pass-unactive');
             document.getElementById("new-pass").disabled = true;
             document.getElementById("password").required = true;
-      }else{
+      } else {
             document.getElementById("new-password").classList.remove('new-pass-unactive');
             document.getElementById("new-pass").disabled = false;
             document.getElementById("password").required = false;
       }
 }
 
-function displayPedidos(){
-      if (document.getElementById("tabla-pedidos").classList.contains('tabla-pedidos-unactive')){
-            document.getElementById("tabla-pedidos").classList.remove('tabla-pedidos-unactive');
-      }else{
-            document.getElementById("tabla-pedidos").classList.add('tabla-pedidos-unactive');
-      }
-}
+document.addEventListener("DOMContentLoaded", () => {
+      document.getElementById('pedidos-recibidos').addEventListener("click", () => {
+            if (document.getElementById("tabla-pedidos-container").classList.contains('oculto')) {
+                  alias = new URLSearchParams(window.location.search).get('alias')
+                  fetch("/listRequestsTo?to_alias=" + alias)
+                        .then(Response => {
+                              if (!Response.ok) {
+                                    throw new Error('Hubo un error en la respuesta del servidor');
+                              }
+                              return Response.text();
+                        })
+                        .then(tablaPedidos => {
+                              document.getElementById('tabla-pedidos-container').innerHTML = tablaPedidos;
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+                        .catch(error => {
+                              console.error('Error obteniendo los datos de los pedidos; ', error)
+                              document.getElementById('tabla-pedidos-container').innerHTML = "<p>Error obteniendo datos</p>"
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+            } else {
+                  document.getElementById("tabla-pedidos-container").classList.add('oculto');
+            }
+      })
+      document.getElementById('pedidos-realizados').addEventListener("click", () => {
+            if (document.getElementById("tabla-pedidos-container").classList.contains('oculto')) {
+                  alias = new URLSearchParams(window.location.search).get('alias')
+                  fetch("/listRequestsFrom?from_alias=" + alias)
+                        .then(Response => {
+                              if (!Response.ok) {
+                                    throw new Error('Hubo un error en la respuesta del servidor');
+                              }
+                              return Response.text();
+                        })
+                        .then(tablaPedidos => {
+                              document.getElementById('tabla-pedidos-container').innerHTML = tablaPedidos;
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+                        .catch(error => {
+                              console.error('Error obteniendo los datos de los pedidos; ', error)
+                              document.getElementById('tabla-pedidos-container').innerHTML = "<p>Error obteniendo datos</p>"
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+            } else {
+                  document.getElementById("tabla-pedidos-container").classList.add('oculto');
+            }
+      })
+      document.getElementById('tabla-pedidos-container').addEventListener("click", (event) => {
+            const link = event.target.closest('a.descartar-pedido-recibido');
+            // Si se encontró el enlace, ejecuta la lógica
+            if (link) {
+                  event.preventDefault(); // Evita que el enlace recargue la página
+                  const alias_from = link.dataset.aliasFrom; // Obtiene el alias desde el data-attribute
+                  const alias_to = link.dataset.aliasTo; // Obtiene el alias desde el data-attribute
+                  fetch(`/deleteRequestsTo?from_alias=${alias_from}&to_alias=${alias_to}`)
+                        .then(Response => {
+                              if (!Response.ok) {
+                                    throw new Error('Hubo un error en la respuesta del servidor');
+                              }
+                              return Response.text();
+                        })
+                        .then(tablaPedidos => {
+                              document.getElementById('tabla-pedidos-container').innerHTML = tablaPedidos;
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+                        .catch(error => {
+                              console.error('Error obteniendo los datos de los pedidos; ', error)
+                              document.getElementById('tabla-pedidos-container').innerHTML = "<th><tr><td>Error obteniendo datos</td></tr></th>"
+                              document.getElementById("tabla-pedidos-container").classList.remove('oculto');
+                        })
+            }
+      })
+})
+
 
 window.onload = function() {
       const urlParams = new URLSearchParams(window.location.search);
       const error = urlParams.get('error');
- 
+
       switch (error) {
             case 'alias_not_found':
-                  alert("El Alias ingresado no se encuentra registrado en la base"); 
+                  alert("El Alias ingresado no se encuentra registrado en la base");
                   break
             case 'password_incorrect':
                   alert("La contraseña ingresada es incorrecta")
@@ -57,18 +124,18 @@ window.onload = function() {
 };
 // Función única para mostrar el formulario seleccionado y ocultar los demás
 function mostrarFormulario(formId) {
-const forms = document.querySelectorAll('.operation-form');
-forms.forEach(form => {
-      if (form.id === formId) {
-            // Si es el formulario que queremos mostrar, le añadimos la clase
-            form.classList.add('form-visible');
-      } else {
-            // A todos los demás, se la quitamos
-            form.classList.remove('form-visible');
-      }
-});
+      const forms = document.querySelectorAll('.operation-form');
+      forms.forEach(form => {
+            if (form.id === formId) {
+                  // Si es el formulario que queremos mostrar, le añadimos la clase
+                  form.classList.add('form-visible');
+            } else {
+                  // A todos los demás, se la quitamos
+                  form.classList.remove('form-visible');
+            }
+      });
 }
 
-function recargarPagina(){
+function recargarPagina() {
       location.reload();
 }
