9d10ec4a9f524e24eead32429ceb0604e54202ce
nachoperna <ignacioperna@gmail.com>
2025-11-05 00:25:50 -0300

    agregado de ordenamiento por alias

--- TP_Especial/db/queries/money_requests.sql
+++ TP_Especial/db/queries/money_requests.sql
@@ -8,11 +8,13 @@ from money_requests
 where to_alias = $1
 ORDER BY
       -- Ordenamiento ascendente
-      CASE WHEN sqlc.arg('sort_by') = 'date' AND sqlc.arg('sort_order') = 'asc' THEN date END ASC,
+      CASE WHEN sqlc.arg('sort_by') = 'from_alias' AND sqlc.arg('sort_order') = 'asc' THEN from_alias END ASC,
       CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'asc' THEN amount END ASC,
+      CASE WHEN sqlc.arg('sort_by') = 'day' AND sqlc.arg('sort_order') = 'asc' THEN date END ASC,
       -- Ordenamiento descendente
-      CASE WHEN sqlc.arg('sort_by') = 'date' AND sqlc.arg('sort_order') = 'desc' THEN date END DESC,
-      CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'desc' THEN amount END DESC;
+      CASE WHEN sqlc.arg('sort_by') = 'from_alias' AND sqlc.arg('sort_order') = 'desc' THEN from_alias END DESC,
+      CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'desc' THEN amount END DESC,
+      CASE WHEN sqlc.arg('sort_by') = 'day' AND sqlc.arg('sort_order') = 'desc' THEN date END DESC;
 
 -- name: GetRequestsFrom :many
 select from_alias, to_alias, amount, to_char(date, 'DD-MM-YYYY') as day, to_char(date, 'HH24:MI') as time, message 
@@ -20,11 +22,13 @@ from money_requests
 where from_alias = $1
 ORDER BY
       -- Ordenamiento ascendente
-      CASE WHEN sqlc.arg('sort_by') = 'date' AND sqlc.arg('sort_order') = 'asc' THEN date END ASC,
+      CASE WHEN sqlc.arg('sort_by') = 'from_alias' AND sqlc.arg('sort_order') = 'asc' THEN from_alias END ASC,
       CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'asc' THEN amount END ASC,
+      CASE WHEN sqlc.arg('sort_by') = 'day' AND sqlc.arg('sort_order') = 'asc' THEN date END ASC,
       -- Ordenamiento descendente
-      CASE WHEN sqlc.arg('sort_by') = 'date' AND sqlc.arg('sort_order') = 'desc' THEN date END DESC,
-      CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'desc' THEN amount END DESC;
+      CASE WHEN sqlc.arg('sort_by') = 'from_alias' AND sqlc.arg('sort_order') = 'desc' THEN from_alias END DESC,
+      CASE WHEN sqlc.arg('sort_by') = 'amount' AND sqlc.arg('sort_order') = 'desc' THEN amount END DESC,
+      CASE WHEN sqlc.arg('sort_by') = 'day' AND sqlc.arg('sort_order') = 'desc' THEN date END DESC;
 
 -- name: DeleteRequest :one
 delete from money_requests where from_alias = $1 and to_alias = $2 
