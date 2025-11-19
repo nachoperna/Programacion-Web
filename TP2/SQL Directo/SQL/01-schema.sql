0129cb4d62836a4825f4297838b419dc308a6a54
nachoperna <ignacioperna@gmail.com>
2025-11-18 23:59:54 -0300

    limpieza de archivos

--- TP2/SQL Directo/SQL/01-schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-      id serial primary key,
-      name varchar(255) not null,
-      email varchar(255) not null,
-      created_at timestamp with time zone default current_timestamp
-);
--- TP_Especial_TP3/go.mod
+++
@@ -1,5 +0,0 @@
-module TP_Especial
-
-go 1.24.7
-
-require github.com/lib/pq v1.10.9 // direct
