1596b1c379c5238079753fe1bb4d781d6a77bf4a
nachoperna <ignacioperna@gmail.com>
2025-11-27 14:55:40 -0300

    contenido de tpespecial traido a raiz

--- TP_Especial/db/migrations/000001_functions.down.sql
+++
@@ -1,2 +0,0 @@
-drop trigger if exists tg_createAccount on users; 
-drop trigger if exists tg_deleteAccount on users; 
--- TP_Especial/structs/movements_struct.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-import "time"
-
-type Movimientos struct {
-	Type   string    `json:"type"`
-	Amount string    `json:"amount"`
-	Date   time.Time `json:"day"`
-}
